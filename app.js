7d98e4955eae91aac1e0b45fdb241c5b5550a760
diptowebhero <diptodas1672@gmail.com>
2022-05-24 10:53:32 +0600

    adedd

--- app.js
+++ app.js
@@ -1,67 +1,102 @@
-// //step-1
-// const smartPhone = {
-//   name: "I phone 12 pro",
-//   price: 120000,
-//   quantity: 2,
+//Object Literal with method
+// const rect = {
+//   width:100,
+//   height:50,
 
-//   totalPrice: function () {
-//     const total = this.price * this.quantity;
-//     console.log(`Total price of ${this.quantity} ${this.name} ${total}`);
+//   draw:function(){
+//     console.log(`i am a rectangle`);
+//     this.printProperties();
 //   },
-// };
-// smartPhone.totalPrice();
+//   printProperties :function(){
+//     console.log(`My width is ${this.width}`);
+//     console.log(`My height is ${this.height}`);
+//   }
+// }
+// rect.draw();
 
-// //step-2 Factory pattern
-// function headPhone(name, price, quantity) {
+//Cerate class using Factory pattern
+//the clean code without repeat
+// function createRect(width, height) {
 //   return {
-//     name,
-//     price,
-//     quantity,
+//     width,
+//     height,
 
-//     totalPrice: function () {
-//       const total = this.price * this.quantity;
-//       console.log(`Total price of ${this.quantity} ${this.name} ${total}`);
+//     draw: function () {
+//       console.log("Hello I am Rectangle");
+//       this.printProperties();
+//     },
+
+//     printProperties: function () {
+//       console.log(`My width is ${this.width}`);
+//       console.log(`My height is ${this.height}`);
 //     },
 //   };
 // }
+// const rect1 = createRect(200, 40);
+// rect1.draw();
+// const rect2 = createRect(300, 50);
+// rect2.draw();
 
-// console.log(headPhone("headphone", 500, 5));
+//Cerate class using Constructor pattern
+// function Rectangle(width, height) {
+//   this.width = width;
+//   this.height = height;
 
-// //step-3 Constructor pattern
+//   this.draw = function () {
+//     console.log("Hello I am Rectangle");
+//     this.printProperties();
+//   };
+//   this.printProperties = function () {
+//     console.log(`My width is ${this.width}`);
+//     console.log(`My height is ${this.height}`);
+//   };
+// }
+
+// const rect3 = new Rectangle(200, 70);
+// console.log(rect3);
+// console.log(rect3.draw());
+
+//Another Constructor function
 // function Product(name, price, quantity) {
 //   this.name = name;
 //   this.price = price;
 //   this.quantity = quantity;
 
 //   this.totalPrice = function () {
-//     const total = this.price * this.quantity;
-//     console.log(`Total price ${this.quantity} ${this.name} ${total}`);
+//     return (total = this.price * this.quantity);
 //   };
 // }
-// const result = new Product('earphone',350,4);
-// console.log(result);
-// const result1 = new Product('Monitor',17500,1);
-// console.log(result1);
 
-// function Add(a, b) {
-//   this.a = a;
-//   this.b = b;
-//   const sum = function () {
-//     return this.a + this.b
-//   }.bind(this);
-//   this.calculator=function(){
-//     console.log(sum);
-//   }
-  
-// }
-// const cal1 =new Add(20,10);
-// console.log(cal1);
+// const product1 = new Product("Samsung", 14500, 3);
+// console.log(product1);
 
-const num = {
-  a:10,
-  b:20
+// Constructor function
+// const product1 = new Function(
+//   'name',
+//   'price',
+//   'quantity',
+//   `
+// this.name = name;
+//   this.price = price;
+//   this.quantity = quantity;
+
+//   this.totalPrice = function () {
+//     return (total = this.price * this.quantity);
+//   };
+// `
+// );
+// const product2 = new product1("Samsung", 14500, 3);
+// console.log(product2.totalPrice());
+
+
+//call,apply,bind
+
+function myFunc(c,d){
+  console.log(this.a + this.b + c + d);
 }
-function sum(c){
-  console.log(this.a + this.b + c);
-}
-sum.call(num,30)
+
+// myFunc.call({a:20,b:30},5,20);
+// myFunc.apply({a:20,b:30},[5,20]);
+
+const result = myFunc.bind({a:20,b:30});
+result(5,20);
